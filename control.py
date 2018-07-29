6d65ca5f4b443eb16d0c94b72df392b734695121
christian <christian@localhost>
2018-07-28 21:34:25 -0700

    set initial default settings

--- control.py
+++ control.py
@@ -1,5 +1,7 @@
 import wx
 
+from default_settings import *
+
 
 class EventsFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
@@ -53,28 +55,32 @@ class LayoutFrame(EventsFrame):
         sizer = wx.GridBagSizer(3, 3)
         panel.SetSizer(sizer)
         data_box = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
-        sizer.Add(data_box, pos=(1, 1), span=(32, 2), flag=wx.EXPAND)
+        data_box.SetValue(DEFAULT_REQUEST_DATA)
+        sizer.Add(data_box, pos=(0, 1), span=(30, 2), flag=wx.EXPAND)
 
         sizer.AddGrowableCol(1)
-        sizer.AddGrowableRow(32)
+        sizer.AddGrowableRow(18)
 
         run_btn = wx.Button(panel, label="Run", size=(120,30))
-        sizer.Add(run_btn, pos=(1, 4))
+        sizer.Add(run_btn, pos=(0, 4))
         stop_btn = wx.Button(panel, label="Stop", size=(120,30))
-        sizer.Add(stop_btn, pos=(1,5))
+        sizer.Add(stop_btn, pos=(0,5))
         cnt_btn = wx.Button(panel, label="Continue", size=(120, 30))
-        sizer.Add(cnt_btn, pos=(1,6))
+        sizer.Add(cnt_btn, pos=(0,6))
 
         host_lbl = wx.StaticText(panel, label="Host:")
         sizer.Add(host_lbl, pos=(3,4))
         host_txt = wx.TextCtrl(panel)
+        host_txt.SetValue(DEFAULT_HOST)
         sizer.Add(host_txt, pos=(3, 5), span=(1, 2), flag=wx.EXPAND)
 
         port_lbl = wx.StaticText(panel, label="Port:")
         sizer.Add(port_lbl, pos=(4, 4))
         port_txt = wx.TextCtrl(panel)
+        port_txt.SetValue(DEFAULT_PORT)
         sizer.Add(port_txt, pos=(4, 5), flag=wx.EXPAND)
         ssl_chk = wx.CheckBox(panel, label="HTTPS")
+        ssl_chk.SetValue(DEFAULT_HTTPS)
         sizer.Add(ssl_chk, pos=(4, 6))
 
         sep1 = wx.StaticLine(panel, size=(300, 1))
@@ -85,6 +91,7 @@ class LayoutFrame(EventsFrame):
         mode_names = ['Serial', 'Concurrent', 'Multiplex', 'Permuter']
         mode_cb = wx.ComboBox(panel, choices=mode_names,
                          style=wx.CB_READONLY)
+        mode_cb.SetValue(DEFAULT_MODE)
         sizer.Add(mode_cb, pos=(8, 5), span=(1, 2))
 
         mark_lbl = wx.StaticText(panel, label="Marker #:")
@@ -92,6 +99,7 @@ class LayoutFrame(EventsFrame):
         mark_numbers = [str(i) for i in range(1, 17)]
         mark_cb = wx.ComboBox(panel, choices=mark_numbers,
                               style=wx.CB_READONLY)
+        mark_cb.SetValue(mark_numbers[0])
         sizer.Add(mark_cb, pos=(9, 5))
 
         load_ps_btn = wx.Button(panel, label="Load File", size=(90, 30))
@@ -108,70 +116,83 @@ class LayoutFrame(EventsFrame):
         sizer.Add(encdr_lbl, pos=(16, 4), span=(1, 1))
         encdrs = ["None", "Hexadecimal", "Base 64", "MD5"]
         encdr_cb = wx.ComboBox(panel, choices=encdrs, style=wx.CB_READONLY)
+        encdr_cb.SetValue(encdrs[0])
         sizer.Add(encdr_cb, pos=(16, 5), span=(1, 2), flag=wx.EXPAND)
 
         sep2 = wx.StaticLine(panel, size=(300, 1))
         sizer.Add(sep2, pos=(18, 4), span=(1, 4), flag=wx.ALIGN_CENTER)
 
-        update_cn = wx.CheckBox(panel, label="Update Content Length Header")
-        sizer.Add(update_cn, pos=(20, 4), span=(1, 3))
+        update_cl = wx.CheckBox(panel, label="Update Content Length Header")
+        update_cl.SetValue(DEFAULT_UPDATE_CL)
+        sizer.Add(update_cl, pos=(20, 4), span=(1, 3))
 
         ka_cb = wx.CheckBox(panel, label="Keep-Alive")
+        ka_cb.SetValue(DEFAULT_KEEP_ALIVE)
         sizer.Add(ka_cb, pos=(21, 4), span=(1, 3))
 
         use_proxy = wx.CheckBox(panel, label="Use Proxy")
+        use_proxy.SetValue(DEFAULT_USE_PROXY)
         sizer.Add(use_proxy, pos=(22, 4), span=(1, 3))
 
         host_lbl = wx.StaticText(panel, label="Proxy Host:")
         sizer.Add(host_lbl, pos=(23,4), span=(1, 1))
         host_txt = wx.TextCtrl(panel)
+        host_txt.SetValue(DEFAULT_PROXY_HOST)
         sizer.Add(host_txt, pos=(23, 5), span=(1, 2), flag=wx.EXPAND)
 
         phost_lbl = wx.StaticText(panel, label="Proxy Port:")
         sizer.Add(phost_lbl, pos=(24,4), span=(1, 1))
         phost_txt = wx.TextCtrl(panel)
+        phost_txt.SetValue(DEFAULT_PROXY_PORT)
         sizer.Add(phost_txt, pos=(24, 5), span=(1, 2), flag=wx.EXPAND)
 
         use_proxy = wx.CheckBox(panel, label="Authenticate Proxy")
+        use_proxy.SetValue(DEFAULT_AUTHENTICATE_PROXY)
         sizer.Add(use_proxy, pos=(25, 4), span=(1, 3))
 
         user_lbl = wx.StaticText(panel, label="Proxy User:")
         sizer.Add(user_lbl, pos=(26,4), span=(1, 1))
         user_txt = wx.TextCtrl(panel)
+        user_txt.SetValue(DEFAULT_PROXY_USER)
         sizer.Add(user_txt, pos=(26, 5), span=(1, 2), flag=wx.EXPAND)
 
         pass_lbl = wx.StaticText(panel, label="Proxy Pass:")
         sizer.Add(pass_lbl, pos=(27,4), span=(1, 1))
         pass_txt = wx.TextCtrl(panel)
+        pass_txt.SetValue(DEFAULT_PROXY_PASS)
         sizer.Add(pass_txt, pos=(27, 5), span=(1, 2), flag=wx.EXPAND)
 
-        nra_lbl = wx.StaticText(panel, label="Network Reconnect Attempts:")
-        sizer.Add(nra_lbl, pos=(28,4), span=(1, 2))
-        nra_txt = wx.TextCtrl(panel)
-        sizer.Add(nra_txt, pos=(28, 6), span=(1, 1), flag=wx.EXPAND)
+        ra_lbl = wx.StaticText(panel, label="Reconnect Attempts:")
+        sizer.Add(ra_lbl, pos=(28,4), span=(1, 2))
+        ra_txt = wx.TextCtrl(panel)
+        ra_txt.SetValue(DEFAULT_NRA)
+        sizer.Add(ra_txt, pos=(28, 6), span=(1, 1), flag=wx.EXPAND)
 
-        nrd_lbl = wx.StaticText(panel, label="Network Reconnect Delay:")
-        sizer.Add(nrd_lbl, pos=(29,4), span=(1, 2))
-        nrb_txt = wx.TextCtrl(panel)
-        sizer.Add(nrb_txt, pos=(29, 6), span=(1, 1), flag=wx.EXPAND)
+        rd_lbl = wx.StaticText(panel, label="Reconnect Delay (ms):")
+        sizer.Add(rd_lbl, pos=(29,4), span=(1, 2))
+        rd_txt = wx.TextCtrl(panel)
+        rd_txt.SetValue(DEFAULT_NRD)
+        sizer.Add(rd_txt, pos=(29, 6), span=(1, 1), flag=wx.EXPAND)
 
-        ct_lbl = wx.StaticText(panel, label="Connection Threads:")
-        sizer.Add(ct_lbl, pos=(30,4), span=(1, 2))
+        nt_lbl = wx.StaticText(panel, label="Network Threads:")
+        sizer.Add(nt_lbl, pos=(30,4), span=(1, 2))
         ct_txt = wx.TextCtrl(panel)
+        ct_txt.SetValue(DEFAULT_THREADS)
         sizer.Add(ct_txt, pos=(30, 6), span=(1, 1), flag=wx.EXPAND)
 
-        dbr_lbl = wx.StaticText(panel, label="Delay Between Requests:")
-        sizer.Add(dbr_lbl, pos=(31,4), span=(1, 2))
+        rd_lbl = wx.StaticText(panel, label="Requests Delay (ms):")
+        sizer.Add(rd_lbl, pos=(31,4), span=(1, 2))
         dbr_txt = wx.TextCtrl(panel)
+        dbr_txt.SetValue(DEFAULT_DBR)
         sizer.Add(dbr_txt, pos=(31, 6), span=(1, 1), flag=wx.EXPAND)
 
         add_mark_btn = wx.Button(panel, label="Add Markers", size=(120, 30))
-        sizer.Add(add_mark_btn, pos=(33, 1), flag=wx.ALIGN_RIGHT)
+        sizer.Add(add_mark_btn, pos=(30, 1), flag=wx.ALIGN_RIGHT)
         clr_mark_btn = wx.Button(panel, label="Clear Markers", size=(120, 30))
-        sizer.Add(clr_mark_btn, pos=(33, 2))
+        sizer.Add(clr_mark_btn, pos=(30, 2))
 
         progress_bar = wx.Gauge(panel, range=100)
-        sizer.Add(progress_bar, pos=(34, 1), flag=wx.EXPAND, span=(1, 2))
+        sizer.Add(progress_bar, pos=(31, 1), flag=wx.EXPAND, span=(1, 2))
 
         sizer.Add((10, 10), pos=(35, 1))
 
@@ -244,7 +265,7 @@ class WindowFrame(MenuFrame):
         self.InitWindow()
         
     def InitWindow(self):
-        self.SetSize((1200, 880))
+        self.SetSize((860, 860))
         self.SetTitle('Heckler Control')
         self.Center()
 
@@ -256,7 +277,7 @@ class ToolbarFrame(WindowFrame ):
         self.InitToolbar()
 
     def InitToolbar(self):
-        toolbar = wx.ToolBar(self, wx.ID_ANY, style=wx.TB_TEXT | wx.TB_HORIZONTAL, pos=(0, 0))
+        toolbar = wx.ToolBar(self, wx.ID_ANY, pos=(0, 0))
         self.ToolBar = toolbar
         new_tb = toolbar.AddTool(wx.ID_ANY, wx.Bitmap('res/icon/file.png'))
         open_tb = toolbar.AddTool(wx.ID_OPEN, wx.Bitmap('res/icon/folder-open.png'))
